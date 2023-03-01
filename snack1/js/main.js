548dc71bf0ff552065f42bb7a85c1033e7af550e
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-01 11:05:44 +0100

    name User - ciclo While - snack1

---
+++ snack1/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Chiedi all'utente il nome 5 volte - CON CICLO WHILE
+
+let i = 0;
+
+while (i < 5) {
+
+let nameUser = prompt('scrivi il tuo nome');
+i++;
+console.log(nameUser);
+
+
+}
