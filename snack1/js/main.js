db489f2818a3a88e81f9f2c6fe5e03babea12a52
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-02 09:02:44 +0100

    correzione/aggiornamento Snack1

--- snack1/js/main.js
+++ snack1/js/main.js
@@ -1,18 +1,27 @@
 'use strict';
 
-// Chiedi all'utente il numero 5 volte e fai la somma - CON CICLO WHILE
-// nuova impostazione esercizio da correggere 
+// // Chiedi all'utente il numero 5 volte e fai la somma - CON CICLO WHILE
 
-let count = 0
+// Imposto una somma di partenza uguale a zero
+let somma= 0;
 
+// Imposto un inserimento dell'utente con valore partenza = 1
+let insert = 1;
 
-while (count < 5) {
-
-let numberUser = Number(prompt('scrivi un numero'));
-console.log(numberUser);
-count++;
-
+/*lancio un ciclo While che imposta la condizione di chiedere per 5 volte
+l'operazione ad un utente*/
+while (insert <= 5){
+  const input = Number(prompt('inserisci un numero' ));
+  console.log(input);
+  if (!isNaN(input)){
+    somma += input;
+    insert++;
+  }
 }
+
+// richiedo un alert che stampi la somma dei 5 numeri inseriti
+alert(somma);
+
 
 
 
